23e7e77e86277c93783c2062d91adbdad79f3d9a
dev <seydinaalioufall@gmail.com>
2019-05-27 18:08:36 +0000

    mise a jour des migrations

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -1,4 +1,4 @@
-<script src="../assets/js/core/jquery.min.js"></script>
+<script src="asset{{'assets/js/core/jquery.min.js'}}"></script>
   <script src="../assets/js/core/popper.min.js"></script>
   <script src="../assets/js/core/bootstrap.min.js"></script>
   <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
